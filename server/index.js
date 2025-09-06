0a22f0f3b10456fc861be471be9dc6d18a230c01
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-09-06 11:13:33 +0300

    Server/Backend setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/' , (req,res) => {
+    res.send('hello')
+})
+
+app.listen(3030 , () => {
+    console.log('Server is running on port 3030');
+});
+
