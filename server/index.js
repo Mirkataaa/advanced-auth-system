5c0085cf454441a8844d77e6c4a0d0f8ba4d2ce9
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-09-06 17:02:49 +0300

    feat(server): connect to db & setup auth routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-import express from 'express';
+import express from "express";
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
 
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 const app = express();
 
-app.get('/' , (req,res) => {
-    res.send('hello')
-})
-
-app.listen(3030 , () => {
-    console.log('Server is running on port 3030');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
+app.use('/api/auth' , authRoutes );
+
+app.listen(3030, () => {
+  connectDB();
+  console.log("Server is running on port 3030");
+});
