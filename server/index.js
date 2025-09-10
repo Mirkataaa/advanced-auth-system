f72f2b83711f6c6220a400753b12238d631a0bba
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-09-10 11:04:33 +0300

    feat(auth): add check-auth route with token verification

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.route.js';
 
 import { connectDB } from "./db/connectDB.js";
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello");
